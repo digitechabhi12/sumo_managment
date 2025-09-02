b8112bb8c0e12e4cedba96b16076bd111e348c6d
digitechabhi12 <abhishek.jha@sumodigitech.com>
2025-09-02 18:02:57 +0530

    new changes

--- app/portal/webapp/controller/dashboard.controller.js
+++ app/portal/webapp/controller/dashboard.controller.js
@@ -5,35 +5,7 @@ sap.ui.define([
     "use strict";
     return Controller.extend("portal.controller.dashboard", {
         onInit: function () {
-            this._loadViewBasedOnRole();
-        },
-        _loadViewBasedOnRole: function () {  
-            var sRole = "Emp";
-            var oModel = new JSONModel({
-                userRole: sRole
-            });
-            this.getView().setModel(oModel, "user");
-            var oContainer = this.byId("viewContainer");
-            var oExistingView = oContainer.getItems().find(function (item) {
-                return item.getId() === "Main" + sRole;
-            });
-
-            if (oExistingView) {
-                oExistingView.setVisible(true);
-            } else {
-                
-                var oView = sap.ui.view({
-                    id: "Main" + sRole,
-                    viewName: "portal.view." + sRole + "." + sRole,
-                    type: "XML"
-                });
-                oContainer.addItem(oView);
-            }
-            oContainer.getItems().forEach(function (oItem) {
-                if (oItem.getId() !== "Main" + sRole) {
-                    oItem.setVisible(false);
-                }
-            });
+           
         }
     });
 });
